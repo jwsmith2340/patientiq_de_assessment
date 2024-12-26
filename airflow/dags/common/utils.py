8134c8b111e711ccb9a0c3e9dc97bbd9c483b78e
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2024-12-26 15:44:48 -0500

    Removing test connection to airflow metadb

--- airflow/dags/common/utils.py
+++ airflow/dags/common/utils.py
@@ -2,14 +2,6 @@ db_connection = {
     'dbname': 'data',
     'user': 'admin',
     'password': 'admin',
-    'host': 'data-postgres',  # e.g., 'localhost'
-    'port': '5432'   # e.g., '5432'
-}
-
-airflow_connection = {
-    'dbname': 'airflow',
-    'user': 'airflow',
-    'password': 'airflow',
-    'host': 'postgres',  # e.g., 'localhost'
-    'port': '5432'   # e.g., '5432'
+    'host': 'data-postgres',
+    'port': '5432'
 }

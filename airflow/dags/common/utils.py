59cada7fb1d0b4782f6dafdbeece656db831730c
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2024-12-26 14:58:17 -0500

    Creating common\utils.py file for shared consts and util fns

---
+++ airflow/dags/common/utils.py
@@ -0,0 +1,15 @@
+db_connection = {
+    'dbname': 'data',
+    'user': 'admin',
+    'password': 'admin',
+    'host': 'data-postgres',  # e.g., 'localhost'
+    'port': '5432'   # e.g., '5432'
+}
+
+airflow_connection = {
+    'dbname': 'airflow',
+    'user': 'airflow',
+    'password': 'airflow',
+    'host': 'postgres',  # e.g., 'localhost'
+    'port': '5432'   # e.g., '5432'
+}

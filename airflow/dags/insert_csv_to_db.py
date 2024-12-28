9a15ca6462ca86fb63719f9b58300f2760d22822
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2024-12-27 22:28:09 -0500

    Cleaning up top level imports, clean up default_args, add doc_md param to DAG declaration, adding all doc strings, adding logging import and replacing print statements, breaking table creation and csv upload into separate tasks, reducing code in csv upload task to use csv_copy_from() fn, created csv_copy_from() fn.

--- airflow/dags/insert_csv_to_db.py
+++ airflow/dags/insert_csv_to_db.py
@@ -1,16 +1,9 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow.decorators import task, dag
-from airflow.hooks.base import BaseHook
-from airflow.models import Variable
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.operators.python import get_current_context
 
 default_args = {
     "owner": "Data Engineering",
     "retries": 0,
-    "retry_delay": timedelta(seconds=3),
-    "retry_exponential_backoff": True,
-    "max_retry_delay": timedelta(seconds=15),
 }
 
 
@@ -20,98 +13,111 @@ default_args = {
     default_args=default_args,
     schedule=None,
     catchup=False,
+    doc_md=__doc__,
     tags=["csv", "insert", "db"],
 )
 def insert_csv_into_db_dag():
+    """DAG to create SQL tables if they do not exist and insert csv files into the DB"""
     import psycopg2
+    import logging
 
     from common.utils import db_connection
 
     @task()
-    def connection():
+    def sql_table_creation():
+        """Create SQL tables from the table_creation.sql file
+
+            Args:
+                None
+
+            Returns:
+                None
+        """
         try:
             connection = psycopg2.connect(**db_connection)
             cursor = connection.cursor()
 
-            # Create tables if they don't exist
             cursor.execute(
                 open("/opt/airflow/dags/sql_files/table_creation.sql", "r").read()
             )
             connection.commit()
 
-            with open("/opt/airflow/flat_files/genres.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f, "genres", sep=",", null="NULL", columns=("id", "genre")
-                )
-
-            with open("/opt/airflow/flat_files/production_companies.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f,
-                    "production_companies",
-                    sep=",",
-                    null="NULL",
-                    columns=("id", "prod_comp"),
-                )
-
-            with open("/opt/airflow/flat_files/spoken_languages.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f, "languages", sep=",", null="NULL", columns=("id", "lang")
-                )
-
-            with open("/opt/airflow/flat_files/movies.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f,
-                    "movies",
-                    sep=",",
-                    null="NULL",
-                    columns=(
-                        "id",
-                        "budget",
-                        "imdb_id",
-                        "revenue",
-                        "release_date",
-                        "title",
-                    ),
-                )
-            
-            with open("/opt/airflow/flat_files/movie_genre_nm.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f, "movie_genre_nm", sep=",", null="NULL", columns=("movie_id", "genre_id")
-                )
-
-            with open("/opt/airflow/flat_files/movie_language_nm.csv", "r") as f:
-                # Skip the headers
-                next(f)
-                cursor.copy_from(
-                    f, "movie_language_nm", sep=",", null="NULL", columns=("movie_id", "language_id")
-                )
-
-            # Commit the transaction
-            connection.commit()
-            print("Data inserted successfully.")
-
         except Exception as e:
-            print(f"Error: {e}")
-            connection.rollback()  # Rollback in case of an error
+            logging.error(f"Error: {e}")
+            connection.rollback()
         finally:
-            # Close the connection
             if cursor:
                 cursor.close()
             if connection:
                 connection.close()
 
+    @task()
+    def upload_csv_files():
+        """Task to upload all created CSV files to the SQL DB
+        
+            Args:
+                None
+
+            Returns:
+                None
+        """
+        try:
+            connection = psycopg2.connect(**db_connection)
+            cursor = connection.cursor()
+
+            csv_val_list = [
+                ["genres", "genres", ("id", "genre")],
+                ["production_companies", "production_companies", ("id", "prod_comp")],
+                ["spoken_languages", "languages", ("id", "lang")],
+                ["movies", "movies", ("id", "budget", "imdb_id", "revenue", "release_date", "title",)],
+                ["movie_genre_nm", "movie_genre_nm", ("movie_id", "genre_id")],
+                ["movie_language_nm", "movie_language_nm", ("movie_id", "language_id")]
+            ]
+
+            for csv_list in csv_val_list:
+                csv_copy_from(cursor=cursor, file_name=csv_list[0], table_name=csv_list[1], columns=csv_list[2])
+            
+            # Commit the transaction
+            connection.commit()
+            logging.info("Data inserted successfully.")
+
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            connection.rollback()
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    def csv_copy_from(cursor, file_name: str, table_name: str, columns: tuple[str]) -> None:
+        """Reusable fn to handle copy_from programatically to reduce code
+        
+            Args:
+                cursor: psycopg2 cursor object
+                file_name (str): The csv file name to be read
+                table_name (str): The SQL table name to upload to
+                columns (tuple[str]): A tuple of SQL table column names
+
+            Returns:
+                None
+        """
+        with open(f"/opt/airflow/flat_files/{file_name}.csv", "r") as f:
+            next(f)
+            cursor.copy_from(
+                f, table_name, sep=",", null="NULL", columns=columns
+            )
+
     def main() -> None:
-        connection()
+        """Primary entry point for the DAG where the task flow is declared
+
+            Args:
+                None
+
+            Returns:
+                None
+        """
+        sql_table_creation() >> upload_csv_files()
 
     main()
 

9c4c4182b5c28c826da2d6d0340b021eb8af21cf
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2024-12-27 21:03:33 -0500

    Running Black for formatting

--- airflow/dags/insert_csv_to_db.py
+++ airflow/dags/insert_csv_to_db.py
@@ -34,53 +34,53 @@ def insert_csv_into_db_dag():
             cursor = connection.cursor()
 
             # Create tables if they don't exist
-            # cursor.execute(
-            #     open("/opt/airflow/dags/sql_files/table_creation.sql", "r").read()
-            # )
-            # connection.commit()
+            cursor.execute(
+                open("/opt/airflow/dags/sql_files/table_creation.sql", "r").read()
+            )
+            connection.commit()
 
-            # with open("/opt/airflow/flat_files/genres.csv", "r") as f:
-            #     # Skip the headers
-            #     next(f)
-            #     cursor.copy_from(
-            #         f, "genres", sep=",", null="NULL", columns=("id", "genre")
-            #     )
+            with open("/opt/airflow/flat_files/genres.csv", "r") as f:
+                # Skip the headers
+                next(f)
+                cursor.copy_from(
+                    f, "genres", sep=",", null="NULL", columns=("id", "genre")
+                )
 
-            # with open("/opt/airflow/flat_files/production_companies.csv", "r") as f:
-            #     # Skip the headers
-            #     next(f)
-            #     cursor.copy_from(
-            #         f,
-            #         "production_companies",
-            #         sep=",",
-            #         null="NULL",
-            #         columns=("id", "prod_comp"),
-            #     )
+            with open("/opt/airflow/flat_files/production_companies.csv", "r") as f:
+                # Skip the headers
+                next(f)
+                cursor.copy_from(
+                    f,
+                    "production_companies",
+                    sep=",",
+                    null="NULL",
+                    columns=("id", "prod_comp"),
+                )
 
-            # with open("/opt/airflow/flat_files/spoken_languages.csv", "r") as f:
-            #     # Skip the headers
-            #     next(f)
-            #     cursor.copy_from(
-            #         f, "languages", sep=",", null="NULL", columns=("id", "lang")
-            #     )
+            with open("/opt/airflow/flat_files/spoken_languages.csv", "r") as f:
+                # Skip the headers
+                next(f)
+                cursor.copy_from(
+                    f, "languages", sep=",", null="NULL", columns=("id", "lang")
+                )
 
-            # with open("/opt/airflow/flat_files/movies.csv", "r") as f:
-            #     # Skip the headers
-            #     next(f)
-            #     cursor.copy_from(
-            #         f,
-            #         "movies",
-            #         sep=",",
-            #         null="NULL",
-            #         columns=(
-            #             "id",
-            #             "budget",
-            #             "imdb_id",
-            #             "revenue",
-            #             "release_date",
-            #             "title",
-            #         ),
-            #     )
+            with open("/opt/airflow/flat_files/movies.csv", "r") as f:
+                # Skip the headers
+                next(f)
+                cursor.copy_from(
+                    f,
+                    "movies",
+                    sep=",",
+                    null="NULL",
+                    columns=(
+                        "id",
+                        "budget",
+                        "imdb_id",
+                        "revenue",
+                        "release_date",
+                        "title",
+                    ),
+                )
             
             with open("/opt/airflow/flat_files/movie_genre_nm.csv", "r") as f:
                 # Skip the headers

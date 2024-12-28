fd99146bff02eb4802b9232dfede45875ee174c5
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2024-12-27 22:01:39 -0500

    Creating write_csv_file() and string_formatting() utils functions for reusable fn access

--- airflow/dags/common/utils.py
+++ airflow/dags/common/utils.py
@@ -1,3 +1,5 @@
+import csv
+
 db_connection = {
     'dbname': 'data',
     'user': 'admin',
@@ -5,3 +7,23 @@ db_connection = {
     'host': 'data-postgres',
     'port': '5432'
 }
+
+
+def write_csv_file(header: list[str], csv_list: list[list], file_name: str):
+    with open(f"/opt/airflow/flat_files/{file_name}.csv", "w") as f:
+        w = csv.writer(f)
+        w.writerow(header)
+        w.writerows(csv_list)
+
+
+def string_formatting(data_str: str) -> str:
+    """Common string formatting functionality created as a util function
+
+        Args:
+            data_str (str): The string to be formatted
+        
+        Returns:
+            str: The formatted string
+    
+    """
+    return data_str.lower().replace('"', "").replace(",", "").replace("\\", "")
